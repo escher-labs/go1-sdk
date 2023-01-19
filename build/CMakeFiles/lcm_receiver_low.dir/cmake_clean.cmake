0afb45f19dd6472af648a5035a9807c4f9181554
ekviamn <ekim2339@gmail.com>
2023-01-20 07:12:17 +0800

    unitree_legged_sdk

---
+++ build/CMakeFiles/lcm_receiver_low.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lcm_receiver_low.dir/examples/lcm_receiver_low.cpp.o"
+  "lcm_receiver_low.pdb"
+  "lcm_receiver_low"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lcm_receiver_low.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
